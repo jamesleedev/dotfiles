680f991bab9bcefec775171d339eceaba1bc4dd3
James Lee <james@jamesl.dev>
2026-06-30 00:07:21 +0100

    chore: add extra scripts

---
+++ backup-dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp ~/.ideavimrc ./backups/
+cp ~/.config/starship.toml ./backups/.config/
+cp ~/.zshenv ./backups/
+cp ~/.tmux.conf ./backups/
+cp -r ~/.config/zsh/ ./.backups/.config/
+cp -r ~/.config/nvim/ ./backups/.config/
+cp -r ~/.config/foot/ ./backups/.config/
+cp -r ~/.config/alacritty/ ./backups/.config/
+cp -r ~/.config/gammastep/ ./backups/.config/
+cp -r ~/.local/bin/ ./backups/.local/bin/
+cp -r ~/.config/ghostty/ ./backups/.config/
+cp -r ~/.config/kanshi/ ./backups/.config/
+cp -r ~/.config/sway/ ./backups/.config/
+cp -r ~/.config/swaylock/ ./backups/.config/
+cp -r ~/.config/ly/ ./backups/.config/
+cp -r ~/.config/rofi/ ./backups/.config/
+cp -r ~/.config/waybar/ ./backups/.config/
+
---
+++ replace-dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ./.tmux.conf ~/
+cp ./.ideavimrc ~/
+cp ./.zshrc ~/
+cp ./.zshenv ~/
+cp ./.zprofile ~/
+cp -r ./.config/ ~/.config/
+cp -r ./.local/ ~/.local/
+
